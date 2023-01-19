ee2872a21dfd3a80c93db25ba81da75d08a93ca7
mcznh <mariacarolina.mf@gmail.com>
2023-01-19 19:53:25 -0300

    first commit

---
+++ MoovielistApp/MoovielistApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoovielistApp
+//
+//  Created by mariemik on 19/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
